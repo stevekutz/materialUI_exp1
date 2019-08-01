8a3d146b271524beaeccf009960dbf271ee494b2
stevekutz <stkutz@gmail.com>
2019-08-01 15:47:26 -0500

    generating Button style using Hook API

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+// import  {Button, Container} from '@material-ui/core/';
+
+import  Button from '@material-ui/core/Button';
+import  Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+    color: 'dodgerblue',
+    border: '1px solid hotpink',
+  },
+  input: {
+    display: 'none',
+  },
+}));
 
 function App() {
+
+ const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxwidth = 'sm'>
+        <Button variant="contained" className={classes.button}>
+          Default
+      </Button>
+      <Button variant="contained">
+        Default without className
+      </Button>
+      <Button className={classes.button} >
+      Default without variant
+    </Button>
+
+    </Container>
   );
 }
 
